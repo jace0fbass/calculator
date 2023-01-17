4f843f497bf2088f6b2f40f17567dbde71f20c47
Jason <jajones414@gmail.com>
2023-01-16 22:40:55 -0600

    Corrected error from changing ÷ and x symbols.

--- script.js
+++ script.js
@@ -42,10 +42,10 @@ class Calculator {
       case "-":
         computation = prev - current;
         break;
-      case "*":
+      case "x":
         computation = prev * current;
         break;
-      case "/":
+      case "÷":
         computation = prev / current;
         break;
       default:
